22c4587f3c88104ab0349c617c61ac35ee34bf59
Mikkel Troels Kongsted <mtkongsted@gmail.com>
2024-09-09 21:39:13 +0200

    dynamic max length for terminal input

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,21 @@ const cursor = document.querySelector<HTMLSpanElement>("#cursor")!;
 
 hideCursor()
 updateCursorPosition()
+setInputMaxLength()
+
 input.addEventListener("input", updateCursorPosition);
 input.addEventListener("keydown", updateCursorPosition);
 input.addEventListener("keyup", updateCursorPosition);
 input.addEventListener("click", updateCursorPosition);
 input.addEventListener("focus", showCursor);
 input.addEventListener("blur", hideCursor);
+window.addEventListener("resize", setInputMaxLength);
+
+function setInputMaxLength() {
+  const width = input.clientWidth
+  const charWidth = 10
+  input.maxLength = Math.floor(width / charWidth) - 1
+}
 
 function updateCursorPosition() {
   // a lot of this is hardcoded. Figure out a way to fix this
@@ -21,8 +30,6 @@ function updateCursorPosition() {
 
   const cursorPosition = input.selectionStart!;
   const cursorLeft = (prefixChars + cursorPosition) * charWidth + marginPx + offsetPx
-  console.log(cursorPosition)
-  console.log(cursorLeft)
 
   cursor.style.left = cursorLeft + "px";
 
