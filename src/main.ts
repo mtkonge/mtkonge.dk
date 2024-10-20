f62ff70b94a73f5e84595698bf57e6050c197ab0
Mikkel Troels Kongsted <mtkongsted@gmail.com>
2024-10-20 16:08:10 +0200

    run command on enter

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import "./style.css"
 
 const input = document.querySelector<HTMLInputElement>("#terminal-input")!;
 const cursor = document.querySelector<HTMLSpanElement>("#cursor")!;
+const history = document.querySelector<HTMLDivElement>("#history")!;
+const userPrefix = document.querySelector<HTMLDivElement>("#user")!;
 
 hideCursor()
 updateCursorPosition()
@@ -15,6 +17,52 @@ input.addEventListener("focus", showCursor);
 input.addEventListener("blur", hideCursor);
 window.addEventListener("resize", setInputMaxLength);
 
+input.addEventListener("keydown", function(event: KeyboardEvent) {
+  if (event.key === 'Enter') {
+    addHistoryItem(runCommand(input.value))
+  }
+  else if (event.ctrlKey && event.key === 'c') {
+    addHistoryItem("")
+  }
+
+}) 
+
+function runCommand(command: string) {
+  if (command.trim() !== "") {
+    return `${command}: Command not found`
+  }
+  else {
+    return ""
+  }
+}
+
+function addHistoryItem(output: string) {
+  const userPrefixClone = userPrefix.cloneNode(true) as HTMLDivElement
+  userPrefixClone.id = ""
+
+  const command = document.createElement("div")
+  command.innerHTML = input.value
+
+  const userAndCommand = document.createElement("div")
+  userAndCommand.classList.add("user-and-command")
+  userAndCommand.appendChild(userPrefixClone)
+  userAndCommand.appendChild(command)
+
+  const outputElement = document.createElement("div")
+  outputElement.innerHTML = output
+
+
+  const historyItem = document.createElement("div")
+  historyItem.classList.add("history-list")
+
+  historyItem.appendChild(userAndCommand)
+  historyItem.appendChild(outputElement)
+
+  history.appendChild(historyItem)
+  input.value = ""
+}
+
+
 function setInputMaxLength() {
   const width = input.clientWidth
   const charWidth = 10
@@ -26,13 +74,12 @@ function updateCursorPosition() {
   const marginPx = 8
   const offsetPx = 2
   const prefixChars = "guest@mtkonge:~$ ".length
-  const charWidth = 10;
+  const charWidthPx = 10;
 
   const cursorPosition = input.selectionStart!;
-  const cursorLeft = (prefixChars + cursorPosition) * charWidth + marginPx + offsetPx
+  const cursorLeft = (prefixChars + cursorPosition) * charWidthPx + marginPx + offsetPx
 
   cursor.style.left = cursorLeft + "px";
-
 }
 
 function showCursor() {
